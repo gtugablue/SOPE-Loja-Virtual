12b408ee3d49c2dd93c332ca84119a9cb0cf4127
André Lago <asl_mbc_13@hotmail.com>
2015-05-11 15:39:36 +0100

    Stuff in ger_cl

--- ger_cl.c
+++ ger_cl.c
@@ -4,13 +4,13 @@ int main(int argc, char **argv)
 {
 	if(argc != 3)
 	{
-		printf("\n\t%s: Inwvalid number of arguments.\n\tMust be: %s <shared_memory_name> <number_of_clients>\n\n", argv[0], argv[0]);
+		printf("\n\t%s: Invalid number of arguments.\n\tMust be: %s <shared_memory_name> <number_of_clients>\n\n", argv[0], argv[0]);
 		return 1;
 	}
 
-	int mem_key;
+	long mem_key;
 	int shm_id;
-	int num_clients;
+	long num_clients;
 
 	if((parse_int(&mem_key, argv[1], 10) != 0) || (shm_id = shmget(mem_key, SHM_SIZE, 0)) == -1)
 	{
@@ -22,6 +22,39 @@ int main(int argc, char **argv)
 	{
 		printf("\n\t%s - ERROR: Invalid number of clients.\n\tThat value is not a number\n\n", argv[0]);
 		return 1;
+	}
+
+	int temp = 0;
+	int fork_return = 0;
+
+	while(temp++ < num_clients)		// Cycle to create specified number of clients
+	{
+		fork_return = fork();
+
+		if(fork_return == 0)
+			break;
+		else if(fork_return < 0)
+		{
+			printf("\n\t%s - INTERNAL ERROR: Unable to create processes using fork\n\n", argv[0]);
+			return 2;
+		}
+	}
+
+	if(fork_return == 0)	// Child-processes - each represents a client who chooses a "balcao" to be attended at
+	{
+		// TODO Something
+	}
+	else					// Parent-process - must wait for each child to finnish before terminating
+	{
+		int child_status = 0;
+
+		while(wait(&child_status) >= 0)
+		{
+			if(child_status != 0)
+			{
+				return 3;
+			}
+		}
 	}
 
 	return 0;
--- ger_cl.h
+++ ger_cl.h
@@ -4,11 +4,17 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/wait.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include "utils.h"
 
 #define SHM_SIZE 1024*1024	// defines the shared memory as a 1MB region
 
+/*
+ * @return - success(0), invalid arguments(1), internal error like fork(2), child-process error(3)
+ */
 int main(int argc, char **argv);
 
 
