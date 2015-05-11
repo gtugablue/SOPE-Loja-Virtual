c3df9294d9981009fe41071efb22f664794cbf41
Gustavo Rocha da Silva <gtugablue@gmail.com>
2015-05-11 16:42:53 +0100

    Changed shared memory to use provided name.

--- balcao.c
+++ balcao.c
@@ -4,17 +4,19 @@
 #include <linux/limits.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
 #define SHARED_MEM_SIZE	1024 * 1024
 
-int create_shared_memory(int *shm_id);
+int create_shared_memory(const char *name, int *shm_id);
 
 int main(int argc, char *argv[])
 {
 	int shm_id;
-	if (create_shared_memory(&shm_id)) return 1;
+	if (create_shared_memory(argv[1], &shm_id)) return 1;
 
-	if (shmctl(shm_id, IPC_RMID, NULL) == -1)
+	if (shm_unlink(argv[1]) == -1)
 	{
 		printf("Error: shared memory wasn't properly cleaned.\n");
 		return 1;
@@ -24,21 +26,9 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-int create_shared_memory(int *shm_id)
+int create_shared_memory(const char *name, int *shm_id)
 {
-	char pathname[PATH_MAX];
-	if (getcwd(pathname, PATH_MAX) == NULL)
-	{
-		printf("Error: couldn't obtain current path.\n");
-		return 1;
-	}
-	key_t shared_mem_key = ftok(pathname, 0);
-	if (shared_mem_key == -1)
-	{
-		printf("Error: couldn't obtain a key for the shared memory\n.");
-		return 1;
-	}
-	if ((*shm_id = shmget(shared_mem_key, SHARED_MEM_SIZE, IPC_CREAT | IPC_EXCL)) == -1)
+	if ((*shm_id = shm_open(name, O_CREAT | O_EXCL | O_RDWR, 0600)) == -1)
 	{
 		printf("Error: couldn't create shared memory.\n");
 		return 1;
